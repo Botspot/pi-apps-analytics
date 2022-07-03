274b7e5e92124132cda85285a36b7ff7910622a4
theofficialgman <28281419+theofficialgman@users.noreply.github.com>
2022-07-03 14:57:35 -0400

    Update getclicks.sh

--- .github/workflows/getclicks.sh
+++ .github/workflows/getclicks.sh
@@ -10,7 +10,7 @@ daysince="$((($(date +%s)-$(date +%s --date "9/22/2020"))/(3600*24)))"
 
 applist="$(ls /tmp/pi-apps/apps | grep .)"
 # temporarily add apps back to applist that have been removed
-applist="$(echo -e "$applist\nTeamviewer Host\nMinecraft Java")"
+applist="$(echo -e "$applist\nTeamViewer Host\nMinecraft Java")"
 #debug output applist
 echo "$applist"
 
